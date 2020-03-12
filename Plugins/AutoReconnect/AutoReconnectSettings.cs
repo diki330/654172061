33708ff8735b6c176273fd1859b7d2de141a1404
dch0319 <1198755756@qq.com>
2020-03-12 20:54:34 +0800

    2020-03-12 添加jk战留牌

---
+++ Plugins/AutoReconnect/AutoReconnectSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using log4net;
+using Newtonsoft.Json;
+using Triton.Bot.Settings;
+using Triton.Common;
+using Triton.Game.Mapping;
+using Logger = Triton.Common.LogUtilities.Logger;
+
+namespace AutoReconnect
+{
+    /// <summary>Settings for the Stats plugin. </summary>
+    public class AutoReconnectSettings : JsonSettings
+    {
+        private static readonly ILog Log = Logger.GetLoggerInstanceForType();
+
+        private static AutoReconnectSettings _instance;
+
+        /// <summary>The current instance for this class. </summary>
+        public static AutoReconnectSettings Instance
+        {
+            get { return _instance ?? (_instance = new AutoReconnectSettings()); }
+        }
+
+        /// <summary>The default ctor. Will use the settings path "Stats".</summary>
+        public AutoReconnectSettings()
+            : base(GetSettingsFilePath(Configuration.Instance.Name, string.Format("{0}.json", "AutoReconnect")))
+        {
+        }
+    }
+}
