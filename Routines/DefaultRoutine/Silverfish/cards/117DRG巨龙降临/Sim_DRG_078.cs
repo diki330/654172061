111010488ca4fd820afc6937604a357876a37973
dch0319 <1198755756@qq.com>
2020-04-06 10:36:32 +0800

    深潜炸弹

--- Routines/DefaultRoutine/Silverfish/cards/117DRG巨龙降临/Sim_DRG_078.cs
+++ Routines/DefaultRoutine/Silverfish/cards/117DRG巨龙降临/Sim_DRG_078.cs
@@ -4,7 +4,22 @@ namespace HREngine.Bots
     {
         //At the start of your turn, deal 5 damage to ALL minions.
         //在你的回合开始时，对所有随从造成5点伤害。
-
+        public override void onTurnStartTrigger(Playfield p, Minion triggerEffectMinion, bool turnStartOfOwner)
+        {
+            if (turnStartOfOwner == triggerEffectMinion.own)
+            {
+                foreach (Minion m in p.ownMinions)
+                {
+                    if (m.entitiyID == triggerEffectMinion.entitiyID) continue;
+                    if (m.playedThisTurn || m.playedPrevTurn)
+                    {
+                        if (PenalityManager.Instance.ownSummonFromDeathrattle.ContainsKey(m.name)) continue;
+                        p.evaluatePenality += (m.Hp * 2 + m.Angr * 2) * 2;
+                    }
+                }
+                p.allMinionsGetDamage(5);
+            }
+        }
 
     }
 }
